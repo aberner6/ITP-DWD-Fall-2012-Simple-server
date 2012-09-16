de1fe4746f669a899bb0462f9adee3e45243a890
John Schimmel <john.schimmel@gmail.com>
2012-09-16 15:03:55 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## A simple webserver in Python
+
+------
+
+### Requirements
+
+Tested on Python 2.7.x
+Python Setuptools and PIP installed
+
+------
+
+### Setup before initially running server
+
+Before you run the server you must install the Flask module. Flask is the Python framework used in this example web server.
+
+To install Flask run the following command in your Terminal console.
+
+	pip install flask
+
+This will download Flask and all of its requirements (Werkzeug & Jinja2)
+
+------
+
+### Running the server
+
+Download the code. To keep your code organized it helps to put example files in their own directory, creating a directory in ~/Documents called 'itp-dwd-examples' is helpful.
+
+Navigate to the directory via Terminal
+
+	cd ~/Documents/itp-dwd-examples
+
+Run the server with the command
+
+	python web.py
+
+	
---
+++ web_w_form.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os
+
+# Retrieve Flask, our framework
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+# this is our main page
+@app.route("/")
+def index():
+    return """<html><body>
+    Hello World!
+    <br/><br/>
+    <a href='/page2'>Visit page #2</a>
+   	</body></html>"""
+
+
+# this is the 2nd route - can be access with /page2
+@app.route("/page2")
+def page2():
+	return "<html><body><h2>Welcome to page 2</h2><p>This is just amazing!</p></body></html>"
+
+
+# new route will accept both a GET and POST request from the client (ie browser)
+@app.route("/form", methods=["GET","POST"])
+def formsample():
+
+	# Did the client make a POST request?
+	if request.method == "POST":
+
+		# get the form data submitted and store it in a variable
+		user_name = request.form.get('user_name', 'Tim Berners-Lee')
+
+		# return custom HTML using the user submitted data
+		return "<html><body><h1>HELLO %s! What an interesting name.</h1><br><a href='/form'>back to form</a></body><html>" % user_name
+
+	else:
+
+		# client made a GET request for '/form'
+		# return a simple HTML form that POSTs to itself
+		return """<html><body>
+		<form action="/form" method="POST">
+			What's your name? <input type="text" name="user_name" id="user_name"/>
+			<input type="submit" value="submit it"/>
+		</form>
+		</body></html>"""
+
+# start the webserver
+if __name__ == "__main__":
+	app.debug = True
+	app.run()
